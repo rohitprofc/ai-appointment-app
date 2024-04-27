3f319eb85104c7f6dcf86a6f556e99245b3a14d2
Zohaib Shaik <20481A5450@gecgudlavallerumic.in>
2024-04-27 15:14:43 +0530

    Successfully got intothe landing page

--- aiappointment/app/models.py
+++ aiappointment/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    password = models.CharField(max_length=128)  # You should hash passwords before storing them
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    date_of_birth = models.DateField()
+    zip_code = models.CharField(max_length=6)
--- aiappointment/app/views.py
+++ aiappointment/app/views.py
@@ -1,2 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'index.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
